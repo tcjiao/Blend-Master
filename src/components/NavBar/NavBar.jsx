3d71677782bee6dafd3c3ae0deea231ae18ec4a4
zeusjiaotianci <zeusjiaotianci@gmail.com>
2023-08-22 16:07:33 -0700

    readme

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,20 +1,22 @@
 import { Link } from 'react-router-dom';
-import * as userService from '../../utilities/users-service'
+import * as userService from '../../utilities/users-service';
+import './NavBar.css';
 
 export default function NavBar({ user, setUser }) {
-    function handleLogOut() {
-        userService.logOut();
-        setUser(null);
-    }
-    
+  function handleLogOut() {
+    userService.logOut();
+    setUser(null);
+  }
+
   return (
-    <nav>
-      <Link to="/drinks">DrinkListPage</Link>
-      &nbsp; | &nbsp;
-      <Link to="/drinks/new">New Drink</Link>
-      &nbsp;&nbsp;
-      <span>Welcome, {user.name}</span>
-      &nbsp;&nbsp;<Link to="" onClick={handleLogOut}>Log Out</Link>
+    <nav className="navbar">
+      <Link className="navbar-link" to="/drinks">Blend Master</Link>
+      <Link to="/drinks" className="navbar-link">DrinkListPage</Link>
+      <Link to="/drinks/new" className="navbar-link">New Drink</Link>
+      <span className="navbar-user">Welcome, {user.name}</span>
+      <Link to="" className="navbar-link" onClick={handleLogOut}>Log Out</Link>
     </nav>
   );
 }
+
+
